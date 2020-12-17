23cd4a305a8f5459bea940c86c95004a24a8677f
Colin Chan <colinchan02@gmail.com>
2020-12-16 20:00:49 -0800

    Update parse.py

--- parse.py
+++ parse.py
@@ -13,9 +13,7 @@ DATA_SOURCE = "https://www.wuxiaworld.com/novel/rmji/rmji-chapter-"
 NUM_CHAPTERS = 20
 WRITE_FILE = os.path.join(os.getcwd(), "rmji.txt")
 
-def main():
-    """ Main program """
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
                'AppleWebKit/537.11 (KHTML, like Gecko) '
                'Chrome/23.0.1271.64 Safari/537.11',
                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -24,6 +22,8 @@ def main():
                'Accept-Language': 'en-US,en;q=0.8',
                'Connection': 'keep-alive'}
 
+def main():
+    """ Main program """
     if os.path.exists(WRITE_FILE):
         os.remove(WRITE_FILE)
 
@@ -42,8 +42,8 @@ def main():
             content = content.find_all('p')
 
             # Write chapter contents to text file
-            for x in content:
-                f.write(x.text + "\n\n")
+            for line in content:
+                f.write(line.text + "\n\n")
 
             # Current status
             print("Page" + str(chapter))
